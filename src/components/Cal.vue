edab0531f946ac965274d02aeda1aa5b64ddfe45
9033 <28668509+9033@users.noreply.github.com>
2020-02-20 07:54:31 +0900

    Cal : 주석 추가.

--- src/components/Cal.vue
+++ src/components/Cal.vue
@@ -16,7 +16,7 @@ vuex: 하나의 변수를 여러 계층의 컴포넌트에서 공유.
 export default {
   name: 'Cal',
   props:{
-    myname:{
+    myname:{//없으면 myname이 없다고 지랄남.
       type:String,//받은 데이터의 타입을 확인. (없어도 실행됨.)
     },
   },
@@ -35,6 +35,9 @@ export default {
   beforeCreate:function(){
     console.log("Cal Cal beforeCreate");//실행되는 순서: beforeCreate -> data
   },
+  /*
+    $store는 vuex. @/store/index.js에 정의해 놓음.
+  */
   computed: {
     // cnt:this.$store.state.count,//$store를 못읽는다고 함.
     count () {
--- src/views/Cal.vue
+++ src/views/Cal.vue
@@ -3,13 +3,13 @@
 </template>
 
 <script>
-import Cal from '@/components/Cal.vue'
+import Cal from '@/components/Cal.vue' //하위 컴포넌트를 임포트함. (이 컴포넌트와는 파일명과 컴포넌트명이 같고 위치는 다름.)
 
 export default {
   components: {
-    Cal
+    Cal//imported component가 template에 뿐만 아니라 여기에도 있어야함.
   },
-  data(){
+  data(){//정의할 변수를 넣은 Object를 반드시 리턴해야함. (그래서 반드시 함수여야함.)
     console.log("Cal data");
     return{//변수 정의하는 부분
       aaa:"한조2"
