50578ec23ace7791a6388861e958d73ed4973997
9033 <28668509+9033@users.noreply.github.com>
2021-07-17 08:55:22 +0900

    컴포넌트안에 컴포넌트

--- src/components/InlineTemplate.vue
+++ src/components/InlineTemplate.vue
@@ -4,20 +4,37 @@
       :is="MyComponent"
       title="가짜"
     />
+    <a-divider/>
     <component 
       :is="MyComponent"
       title="진짜"
+    />
+    <a-divider/>
+    <OurComponent
+      title="진짜 our"
+    />
+    <a-divider/>
+    <OurComponent
+      title="가짜 our"
     />
   </main>
 </template>
 
 <script>
+const OurComponent = ({ props }) => 
+<main>
+  <p>진짜? { props.title }</p>
+  <p>가짜?</p>
+  <a-input
+    value={props.title}
+  />
+</main>
 
 export default {
   // name: '',
   mixins: [],
   components: {
-
+    OurComponent,
   },
   data () {
     return {
@@ -30,11 +47,15 @@ export default {
     MyComponent(){
       return {
         props: ['title'],
-        render(){
+        render(h){
           return (
-            <div>
+            <main>
               <p>진짜? { this.title }</p>
-            </div>
+              <p>가짜?</p>
+              <a-input
+                value={this.title}
+              />
+            </main>
           )
         }
       }
