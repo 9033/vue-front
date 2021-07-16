931b7a46c66bb9ed5f1475a7fbfc7849fca469c4
9033 <28668509+9033@users.noreply.github.com>
2021-07-17 08:30:55 +0900

    컴포넌트 안에서 컴포넌트 정의 jsx를 따로 설정하지 않았는데 사용 가능함.

---
+++ src/components/InlineTemplate.vue
@@ -0,0 +1,62 @@
+<template>
+  <main>
+    <component 
+      :is="MyComponent"
+      title="가짜"
+    />
+    <component 
+      :is="MyComponent"
+      title="진짜"
+    />
+  </main>
+</template>
+
+<script>
+
+export default {
+  // name: '',
+  mixins: [],
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  model: {},
+  props: [],
+  computed: {
+    MyComponent(){
+      return {
+        props: ['title'],
+        render(){
+          return (
+            <div>
+              <p>진짜? { this.title }</p>
+            </div>
+          )
+        }
+      }
+    }
+  },
+  methods: {
+
+  },
+  watch: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+  },
+  beforeDestroy () {
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Component.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <InlineTemplate/>
+  </main>
+</template>
+
+<script>
+import InlineTemplate from '@/components/InlineTemplate.vue'
+
+export default {
+  // name: '',
+  mixins: [],
+  components: {
+    InlineTemplate,
+  },
+  data () {
+    return {
+
+    }
+  },
+  model: {},
+  props: [],
+  computed: {
+
+  },
+  methods: {
+
+  },
+  watch: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  beforeDestroy () {
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
